98325afd05c56e93f8c7ef69889bccdc33e531eb
Jay Kelley <jekelle@iu.edu>
2026-04-27 17:07:14 -0400

    Fix React not defined error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback, useRef } from 'react'
+import { useState, useMemo, useCallback, useRef, useEffect } from 'react'
 import {
   LineChart, Line, BarChart, Bar, RadarChart, Radar, PolarGrid,
   PolarAngleAxis, XAxis, YAxis, CartesianGrid, Tooltip,
